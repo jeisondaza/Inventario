4160a36acf40dac4c13842ae2f15c8d1f3b1d5d5
Jeison Daza <derera199@gmail.com>
2021-11-12 07:38:46 -0400

    primer commit

---
+++ src/main/java/com/example/Inventario/CicloWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.Inventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicloWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicloWebApplication.class, args);
+	}
+
+}
